f9e1350fa56f2c4cdf0e36ec5cd7a8f1f6713498
Yura-D <dyachuk.yu@gmail.com>
2018-07-23 08:45:16 +0300

    #1: Added cheker of input data.      Some changing in code view.

--- books-pages.py
+++ books-pages.py
@@ -1,63 +1,73 @@
 import pyperclip
 
-pages = int(input("How mach page you want to print: "))
+while True:
+    PAGES = input("How mach page you want to print: ")
+    try:
+        NUM_PAGES = int(PAGES)
+        break
+
+    except ValueError:
+        print("Oops!  That was no numbers.  Try again...")
+        continue
 
 
 def right_sequence(sheet, new_list):
     count = 0
-    while count < 4:      
-        try:
+    papers_in_sheet = 4
+    while count < papers_in_sheet:
+        if len(sheet) > 3:
             seq = [sheet.pop(-1), sheet.pop(0), sheet.pop(0), sheet.pop(-1)]
             new_list.extend(seq)
 
-        except:
+        else:
+            print("\nYour last sheets don't have 4 papers...")
             break
 
         count = count + 1
-        
+
     return new_list
 
 
-all_pages = [x for x in range(1, pages+1)]
+ALL_PAGES = [x for x in range(1, NUM_PAGES+1)]
 
-pages_mod = pages % 4
+PAGES_MOD = NUM_PAGES % 4
 
-if pages_mod != 0:
-    first_part = all_pages[:-pages_mod]
-    second_part = all_pages[-pages_mod:]
+if PAGES_MOD != 0:
+    FIRST_PART = ALL_PAGES[:-PAGES_MOD]
+    SECOND_PART = ALL_PAGES[-PAGES_MOD:]
 else:
-    first_part = all_pages
-    second_part = []
+    FIRST_PART = ALL_PAGES
+    SECOND_PART = []
 
-sorted_first_part = list()
+SORTED_FIRST_PART = []
 
-start = 0
-end = 16
-count = 0
-
-
-while start+1 < first_part[-1]:
-    right_sequence(first_part[start:end], sorted_first_part)
-
-    to_print = sorted_first_part[start:end]
-    count = count + 1
-    print("\n", count, " - ", to_print, 4 * " ", \
-        "min page:", min(to_print), "max page: ", max(to_print))
-
-    start = start + 16
-    end = end + 16
+START = 0
+END = 16
+COUNT_SHEET = 0
 
 
-if second_part != []:
-        print("\n", count, " - ", second_part, 4 * " ", \
-        "min page:", min(second_part), "max page: ", max(second_part))
+while START+1 < FIRST_PART[-1]:
+    right_sequence(FIRST_PART[START:END], SORTED_FIRST_PART)
+
+    TO_PRINT = SORTED_FIRST_PART[START:END]
+    COUNT_SHEET = COUNT_SHEET + 1
+    print("\n", COUNT_SHEET, " - ", TO_PRINT, 4 * " ", \
+    "min page:", min(TO_PRINT), "max page: ", max(TO_PRINT))
+
+    START += 16
+    END += 16
 
 
-printing_list = str(sorted_first_part + second_part)[1:-1]
-print("\n Printing list:\n", printing_list)
+if SECOND_PART != []:
+    print("\nSpecial sheet", " - ", SECOND_PART, 4 *" ",\
+    "min page:", min(SECOND_PART), "max page: ", max(SECOND_PART))
+
+
+PRINTING_LIST = str(SORTED_FIRST_PART + SECOND_PART)[1:-1]
+print("\n Printing list:\n", PRINTING_LIST)
 
 
 try:
-    pyperclip.copy(printing_list)
+    pyperclip.copy(PRINTING_LIST)
 except:
     pass
